fb98b87d39e64dd43621decafc56751c10c1f023
Kyle Koeller <kjkoeller@bsu.edu>
2022-03-29 17:36:53 -0400

    Allowed X, Y, and Title to be Changed
    Allowed the user to change the x and y-axis and the title of the graph.

--- O-C Fit/line_fit.py
+++ O-C Fit/line_fit.py
@@ -121,6 +121,9 @@ def data_fit():
             res = mod.fit()
             f.write(res.summary().as_latex())
     
+    # noinspection PyUnboundLocalVariable
+    # print(res.summary())
+    
     f.write(endtex)
     # writes to the file the end latex code and then saves the file
     f.close()
@@ -129,9 +132,9 @@ def data_fit():
     plt.errorbar(x, y, yerr=y_err, fmt="o", color="black")
     # make the legend always be in the upper right hand corner of the graph
     plt.legend(loc="upper right")
-    plt.xlabel("Epoch Number")
-    plt.ylabel("O-C (days)")
-    plt.title("NSVS 254037 O-C")
+    plt.xlabel(input("X-Label: "))
+    plt.ylabel(input("Y-Label: "))
+    plt.title(input("Title: "))
     plt.grid()
     plt.show()
 
