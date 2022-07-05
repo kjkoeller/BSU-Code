91d4567216b30f433c4ebeedac888892f0c623e0
Kyle Koeller <kjkoeller@bsu.edu>
2022-07-05 18:09:39 -0500

    General Updates
    This update just allows the program to be called on by the comparison selector program directly.

--- APASS Catalog Search/cousins_R.py
+++ APASS Catalog Search/cousins_R.py
@@ -1,7 +1,7 @@
 """
 Author: Kyle Koeller
 Created: 4/13/2022
-Last Updated: 4/20/2022
+Last Updated: 7/5/2022
 Based on this paper: https://arxiv.org/pdf/astro-ph/0609736.pdf
 
 This program calculates a Cousins R (R_c) filter band value from a given Johnson V and B, and g' and r'.
@@ -9,6 +9,7 @@ This program calculates a Cousins R (R_c) filter band value from a given Johnson
 
 import pandas as pd
 import numpy as np
+import APASS_catalog_finder as apass
 
 
 # noinspection PyUnboundLocalVariable
@@ -25,6 +26,7 @@ def main():
     e_beta = 0.03
     gamma = 0.219
 
+    '''
     # import file from Catalog Finder or Catalog Comparison
     input_file = input("Enter the text file name from Catalog Finder or Catalog Comparison: ")
     try:
@@ -36,7 +38,8 @@ def main():
         # prints off the reason for closing the program
         print("File not accessible. Please restart program and enter a valid file name/path.")
         exit()
-
+    '''
+    input_file = apass.main()
     df = pd.read_csv(input_file, header=None, skiprows=[0], sep=",")
 
     # writes the columns from the input file
@@ -65,27 +68,32 @@ def main():
     e_Rc = []
     count = 0
 
+    test = 10.551 + (((0.278 * 0.682) - 0.219 - 10.919 + 10.395) / 1.321)
+    total_Rc(test)
+
     # loop that goes through each value in B to get the total amount of values to be calculated
     for i in B:
         # separates the equation out into more easily readable sections
-        div = (alpha*(float(i) - float(V[count])) - gamma - float(g[count]) + float(r[count]))/beta
+        div = (alpha * (float(i) - float(V[count])) - gamma - float(g[count]) + float(r[count])) / beta
         val = float(V[count]) + div
 
-        b_v = ((float(i) - float(V[count]))*e_alpha)**2
-        v_rc = ((float(V[count]) - val)*e_beta)**2
-        beta_alpha = ((beta - alpha)*float(e_V[count]))**2
+        b_v = ((float(i) - float(V[count])) * e_alpha) ** 2
+        v_rc = ((float(V[count]) - val) * e_beta) ** 2
+        beta_alpha = ((beta - alpha) * float(e_V[count])) ** 2
 
         # full equation given in the cited paper at the top of the program file
-        root = np.sqrt(b_v + v_rc + float(e_g[count])**2 + float(e_r[count])**2 + (alpha*float(e_B[count]))**2 + beta_alpha)
+        root = np.sqrt(b_v + v_rc + float(e_g[count]) ** 2 + float(e_r[count]) ** 2 + (
+                    alpha * float(e_B[count])) ** 2 + beta_alpha)
 
         if isNaN(val) is True:
-            # if the value is nan then append 99.999 to the R_c value and its error to make it obvious that there is no given value
+            # if the value is nan then append 99.999 to the R_c value and its error to make it obvious that there is
+            # no given value
             Rc.append(99.999)
             e_Rc.append(99.999)
         else:
             # if there is a value then format that value with only 2 decimal places otherwise there will be like 8
             Rc.append(format(val, ".2f"))
-            e_Rc.append(format((1/beta)*root, ".2f"))
+            e_Rc.append(format((1 / beta) * root, ".2f"))
         count += 1
 
     # puts all columns into a dataframe for output
@@ -106,6 +114,9 @@ def main():
     # noinspection PyTypeChecker
     final.to_csv(output_file, index=None, sep="\t")
     print("Finished Saving")
+    print()
+
+    return output_file
 
 
 def isNaN(num):
@@ -117,6 +128,10 @@ def isNaN(num):
     """
 
     return num != num
+
+
+def total_Rc(Rc):
+    assert Rc == 10.132071915215745
 
 
 if __name__ == '__main__':
